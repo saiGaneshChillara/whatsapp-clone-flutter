45e3b67b6c5d2b000d9570504d16e7cda2193c8f
cvlnpsg987 <cvlnpsg987@gmail.com>
2025-09-06 21:12:49 +0000

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/colors.dart';
+import 'package:myapp/firebase_options.dart';
 import 'package:myapp/screens/mobile_layout_screen.dart';
 import 'package:myapp/screens/web_layout_screen.dart';
 import 'package:myapp/utils/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
